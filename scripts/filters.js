e8f4bb32ebe6355739ab8bf08810a1a8fd320a27
Sergey Yaskov <yaskovdev@gmail.com>
2024-11-28 23:10:26 +0200

    Small improvements to the `post_permalink` filter.

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,8 +1,10 @@
 const DEFAULT_PRIORITY = 10
 
 hexo.extend.filter.register('post_permalink', data => {
-    const slug = data.slug.replace(/\d{4}-\d{2}-\d{2}-/, '');
-    hexo.log.debug('post_permalink is replacing slug ' + data.slug + ' with ' + slug)
-    data.slug = slug
+    const slug = data.slug.replace(/^\d{4}-\d{2}-\d{2}-/, '');
+    if (data.slug !== slug) {
+        hexo.log.debug(`post_permalink is replacing slug "${data.slug}" with "${slug}"`)
+        data.slug = slug
+    }
     return data
 }, DEFAULT_PRIORITY - 1)
